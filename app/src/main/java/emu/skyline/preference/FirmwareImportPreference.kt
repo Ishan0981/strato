e25b8876f377c1cf2dbe281ae8f01fe03c0b8808
Ishan0981 <145665569+Ishan0981@users.noreply.github.com>
2023-12-23 12:49:48 +0530

    Update FirmwareImportPreference.kt

--- app/src/main/java/emu/skyline/preference/FirmwareImportPreference.kt
+++ app/src/main/java/emu/skyline/preference/FirmwareImportPreference.kt
@@ -24,77 +24,56 @@ import java.io.File
 import java.io.FilenameFilter
 import java.io.IOException
 
-class FirmwareImportPreference @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = androidx.preference.R.attr.preferenceStyle
-) : Preference(context, attrs, defStyleAttr) {
-    private class Firmware(val valid: Boolean, val version: String)
+class FirmwareImportPreference @JvmOverloads constructor(context : Context, attrs : AttributeSet? = null, defStyleAttr : Int = androidx.preference.R.attr.preferenceStyle) : Preference(context, attrs, defStyleAttr) {
+    private class Firmware(val valid : Boolean, val version : String)
 
-    private val firmwarePath =
-        File(context.getPublicFilesDir().canonicalPath + "/switch/nand/system/Contents/registered/")
+    private val firmwarePath = File(context.getPublicFilesDir().canonicalPath + "/switch/nand/system/Contents/registered/")
     private val keysPath = "${context.filesDir.canonicalPath}/keys/"
     private val fontsPath = "${context.getPublicFilesDir().canonicalPath}/fonts/"
 
-    private val documentPicker =
-        (context as ComponentActivity).registerForActivityResult(ActivityResultContracts.OpenDocument()) {
-            it?.let { uri ->
-                val inputZip = context.contentResolver.openInputStream(uri)
-                if (inputZip == null) {
-                    Snackbar.make(
-                        (context as SettingsActivity).binding.root,
-                        R.string.error,
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                    return@registerForActivityResult
-                }
-
-                val cacheFirmwareDir = File("${context.cacheDir.path}/registered/")
-
-                val task: suspend () -> Unit = {
-                    var messageToShow: Int
-
-                    try {
-                        // Unzip in cache dir to not delete previous firmware in case the zip given doesn't contain a valid one
-                        ZipUtils.unzip(inputZip, cacheFirmwareDir)
-
-                        val firmware = isFirmwareValid(cacheFirmwareDir)
-                        messageToShow = if (!firmware.valid) {
-                            R.string.import_firmware_invalid_contents
-                        } else {
-                            // Move file operations to a background thread using a coroutine
-                            launch(Dispatchers.IO) {
-                                firmwarePath.deleteRecursively()
-                                cacheFirmwareDir.copyRecursively(firmwarePath, true)
-                                persistString(firmware.version)
-                                extractFonts(firmwarePath.path, keysPath, fontsPath)
-                            }
-                            CoroutineScope(Dispatchers.Main).launch {
-                                notifyChanged()
-                            }
-                            R.string.import_firmware_success
-                        }
-                    } catch (e: IOException) {
-                        messageToShow = R.string.error
-                    } finally {
-                        cacheFirmwareDir.deleteRecursively()
-                    }
-
-                    Snackbar.make(
-                        (context as SettingsActivity).binding.root,
-                        messageToShow,
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                }
-
-                IndeterminateProgressDialogFragment.newInstance(
-                    context as SettingsActivity,
-                    R.string.import_firmware_in_progress,
-                    task
-                )
-                    .show(context.supportFragmentManager, IndeterminateProgressDialogFragment.TAG)
+    private val documentPicker = (context as ComponentActivity).registerForActivityResult(ActivityResultContracts.OpenDocument()) {
+        it?.let { uri ->
+            val inputZip = context.contentResolver.openInputStream(uri)
+            if (inputZip == null) {
+                Snackbar.make((context as SettingsActivity).binding.root, R.string.error, Snackbar.LENGTH_LONG).show()
+                return@registerForActivityResult
             }
+
+            val cacheFirmwareDir = File("${context.cacheDir.path}/registered/")
+
+            val task : () -> Unit = {
+                var messageToShow : Int
+
+                try {
+                    // Unzip in cache dir to not delete previous firmware in case the zip given doesn't contain a valid one
+                    ZipUtils.unzip(inputZip, cacheFirmwareDir)
+
+                    val firmware = isFirmwareValid(cacheFirmwareDir)
+                    messageToShow = if (!firmware.valid) {
+                        R.string.import_firmware_invalid_contents
+                    } else {
+                        firmwarePath.deleteRecursively()
+                        cacheFirmwareDir.copyRecursively(firmwarePath, true)
+                        persistString(firmware.version)
+                        extractFonts(firmwarePath.path, keysPath, fontsPath)
+                        CoroutineScope(Dispatchers.Main).launch {
+                            notifyChanged()
+                        }
+                        R.string.import_firmware_success
+                    }
+                } catch (e : IOException) {
+                    messageToShow = R.string.error
+                } finally {
+                    cacheFirmwareDir.deleteRecursively()
+                }
+
+                Snackbar.make((context as SettingsActivity).binding.root, messageToShow, Snackbar.LENGTH_LONG).show()
+            }
+
+            IndeterminateProgressDialogFragment.newInstance(context as SettingsActivity, R.string.import_firmware_in_progress, task)
+                .show(context.supportFragmentManager, IndeterminateProgressDialogFragment.TAG)
         }
+    }
 
     init {
         val keysDir = File(keysPath)
@@ -112,7 +91,12 @@ class FirmwareImportPreference @JvmOverloads constructor(
 
     override fun onClick() = documentPicker.launch(arrayOf("application/zip"))
 
-    private fun isFirmwareValid(cacheFirmwareDir: File): Firmware {
+    /**
+     * Checks if the given directory stores a valid firmware. For that, all files must be NCAs and
+     * one of them must store the firmware version.
+     * @return A pair that tells if the firmware is valid, and if so, which firmware version it is
+     */
+    private fun isFirmwareValid(cacheFirmwareDir : File) : Firmware {
         val filterNCA = FilenameFilter { _, dirName -> dirName.endsWith(".nca") }
 
         val unfilteredNumOfFiles = cacheFirmwareDir.list()?.size ?: -1
@@ -124,6 +108,6 @@ class FirmwareImportPreference @JvmOverloads constructor(
         } else Firmware(false, "")
     }
 
-    private external fun fetchFirmwareVersion(systemArchivesPath: String, keysPath: String): String
-    private external fun extractFonts(systemArchivesPath: String, keysPath: String, fontsPath: String)
+    private external fun fetchFirmwareVersion(systemArchivesPath : String, keysPath : String) : String
+    private external fun extractFonts(systemArchivesPath : String, keysPath : String, fontsPath : String)
 }
